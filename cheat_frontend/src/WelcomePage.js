024351e728800587efd2196a813a755d4116e43d
Thomas Gaskin <tgaskin@live.co.uk>
2026-05-11 16:12:13 +0100

    Bugfix: clear gameKey when closing join game box

--- cheat_frontend/src/WelcomePage.js
+++ cheat_frontend/src/WelcomePage.js
@@ -643,7 +643,7 @@ const WelcomePage = ({onGameStart}) => {
                     <div className={`rounded-2xl bg-white ${isLandscape ? '' : 'max-w-md'} w-full shadow-2xl`}>
 
                         <button
-                            onClick={() => setAnimationPhase('buttons-visible')}
+                            onClick={() => { setAnimationPhase('buttons-visible'); setGameKey(''); setJoinError(''); }}
                             className="absolute right-1 top-1 text-gray-500 hover:text-gray-700 transition-colors"
                             type="button"
                         >
